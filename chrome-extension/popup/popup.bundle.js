2abdf69039141ec72dd33a86a909feee11a8b638
Mohammed <info@iropit.com>
2026-05-16 09:44:41 +0400

    feat(extension): rename All to All Devices in SMS tab

--- chrome-extension/popup/popup.bundle.js
+++ chrome-extension/popup/popup.bundle.js
@@ -31575,7 +31575,7 @@ ${this.customData.serverResponse}`;
         <path d="M23 21v-2a4 4 0 00-3-3.87"/>
         <path d="M16 3.13a4 4 0 010 7.75"/>
       </svg>
-      <span>All</span>${allCountHtml}
+      <span>All Devices</span>${allCountHtml}
     </button>
     ${deviceTabsHTML}
   `;
--- chrome-extension/src/services/devices.js
+++ chrome-extension/src/services/devices.js
@@ -473,7 +473,7 @@ export function updateSmsDeviceTabs() {
         <path d="M23 21v-2a4 4 0 00-3-3.87"/>
         <path d="M16 3.13a4 4 0 010 7.75"/>
       </svg>
-      <span>All</span>${allCountHtml}
+      <span>All Devices</span>${allCountHtml}
     </button>
     ${deviceTabsHTML}
   `;
