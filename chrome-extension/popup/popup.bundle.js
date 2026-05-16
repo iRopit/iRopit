54b5ef4c8bdfbc51226cac4e63b49baec13411f2
Mohammed <info@iropit.com>
2026-05-16 09:43:26 +0400

    feat(extension): rename All to All Devices in Chat, Calls, Notifications tabs

--- chrome-extension/popup/popup.bundle.js
+++ chrome-extension/popup/popup.bundle.js
@@ -31511,7 +31511,7 @@ ${this.customData.serverResponse}`;
         <path d="M23 21v-2a4 4 0 00-3-3.87"/>
         <path d="M16 3.13a4 4 0 010 7.75"/>
       </svg>
-      <span>All</span>
+      <span>All Devices</span>
     </button>
     ${deviceTabsHTML}
   `;
@@ -31635,7 +31635,7 @@ ${this.customData.serverResponse}`;
         <path d="M23 21v-2a4 4 0 00-3-3.87"/>
         <path d="M16 3.13a4 4 0 010 7.75"/>
       </svg>
-      <span>All</span>${allCountHtml}
+      <span>All Devices</span>${allCountHtml}
     </button>
     ${deviceTabsHTML}
   `;
@@ -31682,7 +31682,7 @@ ${this.customData.serverResponse}`;
         <path d="M23 21v-2a4 4 0 00-3-3.87"/>
         <path d="M16 3.13a4 4 0 010 7.75"/>
       </svg>
-      <span>All</span>${allCountHtml}
+      <span>All Devices</span>${allCountHtml}
     </button>
     ${deviceTabsHTML}
   `;
