9a6134f480d68aaff09d325bd6fcb49b2cfeee7f
Mohammed <info@iropit.com>
2026-07-14 11:17:00 +0400

    Add GTM container snippet while preserving GA4 and Google Ads tags

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -262,6 +262,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WS827722');",
+          }}
+        />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-KF63PFVEGY" />
         <script
           dangerouslySetInnerHTML={{
@@ -294,6 +300,14 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${cairo.variable} antialiased min-h-screen flex flex-col`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WS827722"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <LanguageProvider>
           <AuthProvider>
             <ServiceWorkerRegister />
